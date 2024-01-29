6ddfd377258446755b3351726d5107e1283889dc
Syaoran Nguyen <jinnguyen054@gmail.com>
2024-01-29 16:48:39 +0700

    Basic Auth with Cookies

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import routes from './routes';
 import { connect } from './config/db';
 import error from './app/middlewares/error';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ connect();
 const PORT = process.env.PORT || 5000;
 const app: Express = express();
 
+app.use(cookieParser(process.env.SECRET_KEY));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
